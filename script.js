e52575e94cac560210175525c0a9cc8930284616
Ali <aliwolfi18@gmail.com>
2020-08-11 03:49:50 -0700

    added manipulating DOM

--- script.js
+++ script.js
@@ -1,14 +1,49 @@
-// Get quote from api 2
+const quoteContainer = document.getElementById('quote--container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
+// Get quote from api
 async function getQuote() {
     const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
-        console.log(data);
+
+        // if author is blanl, add 'unknown'
+        if (data.quoteAuthor === '') {
+            authorText.innerHTML = 'Unknown'
+        } else {
+            authorText.innerHTML = data.quoteAuthor;
+        }
+
+        // reduce font size for quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerHTML = data.quoteText;
     } catch (error) {
         getQuote();
+
     }
 }
+
+
+// Tweet quote
+function tweetQuote() {
+    const quote = quoteText.innerText;
+    const author = authorText.innerHTML;
+    const twitterUrl = 'https://twitter.com/intent/tweet?text=' + quote + ' - ' + author;
+    window.open(twitterUrl,'_blank')
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click',getQuote);
+twitterBtn.addEventListener('click',tweetQuote);
+
 
 getQuote();
