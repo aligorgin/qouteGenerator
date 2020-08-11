b372e07a12b8c3803dd2de5c4d7973e0f0416927
Ali <aliwolfi18@gmail.com>
2020-08-10 17:36:36 -0700

    API getting fixed

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-// get quote from api
+// Get quote from api
 async function getQuote() {
-    const proxyUrl = 'https://cors-anywhere.herokuapp.com/'
-    const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote$lang=eng&format=json';
+    const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+    const apiUrl = 'https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
         const data = await response.json();
         console.log(data);
-    } catch (e) {
-        console.log('whooops, no quote'+ e)
+    } catch (error) {
+        getQuote();
     }
 }
 
-console.log(getQuote());
+getQuote();
